c23c390d34fb5319ed386f6b52995764f36f4e05
NotReliable <99848226+NotReliable@users.noreply.github.com>
2022-08-30 14:19:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ctma3tcwwlju.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jacksiowwoonyew-employee"
+customregion = "us-east-1"
